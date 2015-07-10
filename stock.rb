2439d6849f524acc6a9680ec0b25999cfceefe49
tomobk <tomobk@users.noreply.github.com>
2015-07-10 17:26:53 +0900

    Update stock.rb

--- stock.rb
+++ stock.rb
@@ -8,25 +8,41 @@ require 'curses'
 
 include Curses
 
-#stock code assign
-
 begin
 
-stocks = ARGV[0]
-
 view_thread = Thread.new do
+  
   loop do
 
-   stock = JpStock.price(:code => stocks )
-   name = JpStock.sector(:code => stocks )
+#stock code assign
 
-   print "\033[2K\r#{name.company_name}"
+  stocks = ARGV[0]
+  today = Date.today
+  lastday = today - 1
+  
+  stock = JpStock.price(:code => stocks )
+  name = JpStock.sector(:code => stocks )
+  
+  hstocks = JpStock.historical_prices(
+  :code => stocks,
+  :start_date => lastday,
+  :end_date => Date.today,
+   )
+
+  last_day = hstocks[0].close
+  this_day = stock.close
+  rate = ((this_day - last_day)/last_day * 100).round(2)
+
+  print "\033[2K\r#{name.company_name}"
     sleep 1
-   print "\033[2K\r#{stock.open}## Opening Price!! ##"
+  print "\033[2K\r#{stock.open} ## Opening Price!! ##"
     sleep 1
-   print "\033[2K\r#{stock.close}## Current Price!! ##"
+  print "\033[2K\r#{stock.close} ## Current Price!! ##"
     sleep 1
+  print "\033[2K\r#{rate}% ## The day before ration!! ##"
+
   end
+
 end
 
 input_thread = Thread.new do
