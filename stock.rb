0805ddefa4ded292b5f358420831e37d55f6e4cc
tomobk <tomobk@users.noreply.github.com>
2015-07-13 12:00:15 +0900

    Update stock.rb

--- stock.rb
+++ stock.rb
@@ -5,42 +5,54 @@ require 'rubygems'
 require 'jpstock'
 require 'io/console'
 require 'curses'
+require 'holiday_japan'
 
 include Curses
+
+class Date
+  def business_days(i)
+    date = self
+    i.times.each do |j|
+      date -= 1
+      date -= 1 while (date.wday <= 0 || date.wday >= 6) or (date.national_holiday?)
+    end
+    date
+  end
+end
+
 
 begin
 
 view_thread = Thread.new do
-  
+
   loop do
 
 #stock code assign
 
   stocks = ARGV[0]
   today = Date.today
-  lastday = today - 1
-  
+
   stock = JpStock.price(:code => stocks )
   name = JpStock.sector(:code => stocks )
-  
-  hstocks = JpStock.historical_prices(
-  :code => stocks,
-  :start_date => lastday,
-  :end_date => Date.today,
-   )
+
+    hstocks = JpStock.historical_prices(
+    :code => stocks,
+    :start_date => Date.today.business_days(24),
+    :end_date => Date.today
+    )
 
   last_day = hstocks[0].close
   this_day = stock.close
   rate = ((this_day - last_day)/last_day * 100).round(2)
 
-  print "\033[2K\r#{name.company_name}"
-    sleep 1
-  print "\033[2K\r#{stock.open} ## Opening Price!! ##"
-    sleep 1
-  print "\033[2K\r#{stock.close} ## Current Price!! ##"
-    sleep 1
-  print "\033[2K\r#{rate}% ## The day before ration!! ##"
 
+   print "\033[2K\r#{name.company_name}"
+     sleep 1
+   print "\033[2K\r#{stock.open} ## Opening Price!! ##"
+     sleep 1
+   print "\033[2K\r#{stock.close} ## Current Price!! ##"
+     sleep 1
+   print "\033[2K\r#{rate}% ## The day before ration!! ##"
   end
 
 end
